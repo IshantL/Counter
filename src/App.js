fd99b8e4138a163bd5cf4101db77f7e942933206
ishantl <ishantlambhate@gmail.com>
2021-06-11 10:09:27 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
 import logo from './logo.svg';
+import { useState } from 'react';
 import './App.css';
-
 function App() {
+
+  const [counter, setCounter] = useState(1);
+  const [maxValue, setMaxValue] = useState(1000);
+
+   const increment = () => {
+    if (counter<maxValue )
+     setCounter(counter + 1)
+   }
+   
+   const decrement = () => {
+     setCounter(counter - 1)
+   }
+
+   const handleInputChange =(e) =>{
+     let value = parseInt(e.target.value);
+       setCounter(value);
+   }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <div className="outer-container">
+        <button  className="box-first" onClick={decrement}>-</button>
+        <input type ="number" min="1" max="1000" className="box-second" onChange={handleInputChange} value={counter}/>
+        <button className="box-third" onClick={increment}>+</button>
+        </div>
   );
 }
 
